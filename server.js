712240d19b2e9ef39a19bbc0b853700a9d0e2497
plainsight16 <timmybaks2002@gmail.com>
2020-11-27 11:49:20 +0100

    ssl problem

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ const db = knex ({
   client: 'pg',
   connection: {
    connectionString: process.env.DATABASE_URL,
-   ssl: true
+   ssl: true,
+   process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
   }
 });
 
