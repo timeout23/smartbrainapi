8cbf47393e3ba7a2d8a854201d5c939861eddec0
plainsight16 <timmybaks2002@gmail.com>
2020-11-27 11:59:48 +0100

    still on it

--- server.js
+++ server.js
@@ -10,14 +10,13 @@ const profile = require ('./Controller/profile');
 const image = require ('./Controller/image');
 
 
-
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 const db = knex ({
   client: 'pg',
   connection: {
    connectionString: process.env.DATABASE_URL,
    ssl: true,
-   process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
   }
 });
 
